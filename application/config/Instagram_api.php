34db628000bdf903e67553bbe5a6a4fe9e92cfc8
Mark Goldsmith <mark@goldsmith-mark-imac.chapel.local>
2013-01-27 11:40:15 -0500

    Updated Readme again

--- application/config/Instagram_api.php
+++ application/config/Instagram_api.php
@@ -9,16 +9,13 @@
 |
 */
 
-$config['instagram_client_name']	= 'Movi Live Feed';
-$config['instagram_client_id']		= 'd7ba4d21185140b6a7df072ca830a304';
-$config['instagram_client_secret']	= '35fe76e8f8424d76a1a5cb306114e6db';
-$config['instagram_callback_url']	= 'http://movi.org/livestagram/callback/redirect_uri/';
-$config['instagram_website']		= 'http://movi.org/livestagram/';
+$config['instagram_client_name']	= '';
+$config['instagram_client_id']		= '';
+$config['instagram_client_secret']	= '';
+$config['instagram_callback_url']	= 'http://yoursite.com/livestagram/callback/redirect_uri/';
+$config['instagram_website']		= 'http://yoursite.com/livestagram/';
 $config['instagram_description']	= '';
 
-
-// delete all objects
-//https://api.instagram.com/v1/subscriptions?client_secret=35fe76e8f8424d76a1a5cb306114e6db&object=all&client_id=d7ba4d21185140b6a7df072ca830a304
 
 // There was issues with some servers not being able to retrieve the data through https
 // If you have this problem set the following to FALSE 
